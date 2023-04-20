d7d9ab78cd80a46308ea50457d73e332e562a7cc
d6felix <d6felix@gmail.com>
2023-04-20 17:00:14 +0300

    added storeZone and calcZone

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Button from "./components/Button/Button";
 import classNames from "classnames";
 
 import "./App.scss";
-import { useDrag } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
+import DragElement from "./components/DragElement/DragElement";
+import DropArea from "./components/DropArea/DropArea";
 
 export const ItemTypes = {
     BUTTON: 'button'
 }
 
+type CalcButton = {
+    button: ReactNode;
+    isSelected: boolean;
+}
+
 export const App = () => {
-    let buttons = Array(12).fill(0);
+    let buttons: CalcButton[] = Array.from(Array(12));
+    let storeZone = Array(12).fill(0);
+    let calcZone = Array(12).fill(0);
     const [isSelected, setIsSelected] = useState<boolean[]>(Array(12).fill(false));
+    const [lastSelected, setLastSelected] = useState<number>(0);
 
+    buttons = buttons.map((_, index) => {
+        const button =
+            (<DragElement type={ItemTypes.BUTTON} key={index}>
+                <button
+                    className={classNames("button", isSelected[index] && "button__selected")}
+                    onClick={() => {
+                        setIsSelected([...isSelected.slice(0, index), !isSelected[index], ...isSelected.slice(index + 1)]);
+                    }}
+                >My number: {index}</button >
+            </DragElement>);
+        return ({ button, isSelected: isSelected[index] });
+    });
 
-    buttons = buttons.map((value, index) => {
-        const [{ isDragging }, drag] = useDrag(() => ({
-            type: ItemTypes.BUTTON,
-            collect: (monitor) => ({
-                isDragging: !!monitor.isDragging()
-            })
-        }))
+    storeZone = storeZone.map((_, index) => {
         return (
-            <button
-                ref={drag}
-                key={index}
-                className={classNames("button", isDragging && "button_dragging", isSelected[index] && "button__selected")}
-                onClick={() => {
-                    setIsSelected([...isSelected.slice(0, index), !isSelected[index], ...isSelected.slice(index + 1)]);
-                }}
-            >My number: {index}</button >
+            <DropArea type={ItemTypes.BUTTON} key={index}>
+                {!buttons[index].isSelected && buttons[index].button}
+            </DropArea>
         );
     });
 
+    calcZone = calcZone.map((_, index) => {
+        return (
+            <DropArea type={ItemTypes.BUTTON} key={index}>
+                {buttons[index].isSelected && buttons[index].button}
+            </DropArea>
+        );
+    });
 
     return (
         <>
             <div className={classNames("container")}>
-                {buttons}
+                {storeZone}
             </div>
 
             <div className={classNames("container")}>
-            </div>
+                {calcZone}
+            </div >
         </>
     );
 }
