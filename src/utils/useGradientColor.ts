72584936187102a3cf75ee5237a01d3b113d6795
d6felix <d6felix@gmail.com>
2023-04-21 01:04:53 +0300

    small style fixes

--- src/utils/useGradientColor.ts
+++ src/utils/useGradientColor.ts
@@ -2,12 +2,16 @@ import { Selected } from "src/App";
 import { useMemo } from "react";
 
 export const useGradientColor = (selected: Selected[], colorsTable: string[]): string => useMemo(() => {
-    let selectedColors = selected.filter((item) => item.isSelected)
-        .sort((a, b) => a.position - b.position)
-        .reduce((result, item) => {
-            const index = selected.findIndex(i => i.isSelected && i.position === item.position)
-            return result + ", " + colorsTable[index];
-        }, "");
+    const filteredColors = selected.filter((item) => item.isSelected);
+    if (filteredColors.length >= 2) {
+        let selectedColors = filteredColors.sort((a, b) => a.position - b.position)
+            .reduce((result, item) => {
+                const index = selected.findIndex(i => i.isSelected && i.position === item.position)
+                return result + ", " + colorsTable[index];
+            }, "");
 
-    return `linear-gradient(90deg${selectedColors})`;
+        return `linear-gradient(90deg${selectedColors})`;
+    } else {
+        return "#FFFFFF";
+    }
 }, [selected, colorsTable]);
